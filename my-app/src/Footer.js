db3cad3ea716b9ec07e71312daa393816a2a4a24
Jordan Evans <jordanevans19@gmail.com>
2024-03-21 16:10:56 -0700

    finished styling

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Footer extends React.Component {
     render() {
-      return <footer></footer>;
+      return <footer>
+        <div class="flex-container">
+            <div class="col fourth">
+                <img src="Mario and Adrian A.jpg"></img>
+            </div>
+            <div class="col fourth">
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">Order online</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+                </div>
+                <div class="col fourth address">
+                    <p>123 Fake St, New York</p>
+                    <p>(555) 555-555</p>
+                    <p>info@littlelemon.com</p>
+                </div>
+                <div class="col fourth address">
+                <p>Address</p>
+                    <p>Phone number</p>
+                    <p>Email</p>
+                </div>
+        </div>
+
+      </footer>;
     }
 }
 export default Footer;
--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -29,26 +29,63 @@ class Main extends React.Component {
             <div class="col">
             <img src="greek salad.jpg"></img>
             <div class="text">
-            <p class="title">Greek Salad</p>
-            <p>The famous greek salad of crispy lttuce, peppers, olives and our Chicago
+            <p class="title">Greek Salad &nbsp;&nbsp;&nbsp;<span class="price">$12.99</span></p>
+            <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago
                 style feta cheese, garnished with crunchy garlic and rosemary croutons.
             </p>
+            <br></br>
+            <p class="delivery">Order a delivery</p>
             </div>
             </div>
             <div class="col">
-                <img src="bruchetta.svg"></img>
+                <img src="bruchetta.png"></img>
                 <div class="text">
-                <p class="title">Bruchetta</p>
+                <p class="title">Bruchetta &nbsp;&nbsp;&nbsp;<span class="price">$5.99</span></p>
+                <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasons with salt and olive oil</p>
+                <br></br>
+                <br></br>
+                <p class="delivery">Order a delivery</p>
                 </div>
             </div>
             <div class="col">
             <img src="lemon dessert.jpg"></img>
             <div class="text">
-            <p class="title">Lemon Dessert</p>
+            <p class="title">Lemon Dessert <span class="price">$5.00</span></p>
+            <p>
+                This comes straight from grandma's recipe book, every last ingredient has been sourced and is authentic as can be imagined.
+            </p>
+            <br></br>
+            <p class="delivery">Order a delivery</p>
             </div>
             </div>
         </div>
-
+        <div class="testimonials flex-container">
+        <div class="onecol"><h2>Testimonials</h2></div>
+        <div class="col fourth"><p>Rating:</p></div>
+        <div class="col fourth"><p>Rating:</p></div>
+        <div class="col fourth"><p>Rating:</p></div>
+        <div class="col fourth"><p>Rating:</p></div>
+        </div>
+        <div class="about flex-container">
+            <div class="col">
+        <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <br></br>
+                <p>We are a family owned Mediterranean resturaunt, focused on traditional
+                recipes served with a modern twist. 
+                </p>
+                </div>
+                <div class="col">
+                <div class="images flex-container">
+                <div class="col first"> 
+                    <img class="first" src="restaurant.jpg"></img>
+                    </div>
+                    <div class="col">
+                    <img  class="second"  src="restaurant chef B.jpg"></img>
+                    </div>
+                    </div>
+                </div>
+        </div>
 
       </main>;
     }
